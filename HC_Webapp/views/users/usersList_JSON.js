352e690ebe55aaae769ea7257d00e47387b781e0
Shirel Lalo} <shirulalo2003@gmail.com>
2022-07-21 23:05:57 -0300

    cambiando de jv a json

--- HC_Webapp/views/users/usersList_JSON.js
+++ HC_Webapp/views/users/usersList_JSON.js
@@ -1,43 +1,35 @@
-const userList = [
+/*archivo cambiado de jv a json. el mismo archivo esta en la carpeta data como users.json
+[{
+  "id": 1,
+  "first_name": "Carolina",
+  "last_name": "Shlain",
+  "user_name":"cshlain",
+  "passwd": "123",
+  "role":"admin",
+  "loggedIn": false
+},
   {
-    id: 1,
-    first_name: 'Carolina',
-    last_name: 'Shlain',
-    user_name: 'cshlain',
-    passwd: '123',
-    role: 'admin',
-    loggedIn: false,
+    "id": 2,
+    "first_name": "Shirel",
+    "last_name": "Lalo",
+    "user_name":"slalo",
+    "passwd": "123",
+    "role":"admin",
+    "loggedIn": false
   },
   {
-    id: 2,
-    first_name: 'Shirel',
-    last_name: 'Lalo',
-    user_name: 'slalo',
-    passwd: '123',
-    role: 'admin',
-    loggedIn: false,
-  },
-  {
-    id: 3,
-    first_name: 'Walter',
-    last_name: 'Mattioz',
-    user_name: 'wmattioz',
-    passwd: '123',
-    role: 'admin',
-    loggedIn: false,
-  },
-  {
-    id: 4,
-    first_name: 'Fernando',
-    last_name: 'Grandoli',
-    user_name: 'fgrandoli',
-    passwd: '123',
-    role: 'admin',
-    loggedIn: false,
-  },
+    "id": 3,
+    "first_name": "Fernando",
+    "last_name": "Grandoli",
+    "user_name":"fgrandoli",
+    "passwd": "123",
+    "role":"admin",
+    "loggedIn": false
+  }
+
 ];
 
-module.exports = userList;
+
 
 /* TEMPLATE:
 
