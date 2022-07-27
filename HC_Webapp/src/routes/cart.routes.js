536422739959f6ec65e0ca8bfae13696dfc905e1
CarolinaShlain123 <caroshlain@gmail.com>
2022-07-27 18:38:29 -0300

    carrito

--- HC_Webapp/src/routes/cart.routes.js
+++ HC_Webapp/src/routes/cart.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const controllerCart = require('../controllers/controller.cart.js');
 
 router.get('/', controllerCart.mostrarCart);
-router.get('/', controllerCart.mostrarCart);
 router.get('/:id', controllerCart.mostrarCart);
 
 module.exports = router;
