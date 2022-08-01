485ce5923674840418cddc857d3c4663d4fd594f
CarolinaShlain123 <caroshlain@gmail.com>
2022-08-01 17:43:50 -0300

    intentando hacer views de cart

--- HC_Webapp/src/controllers/controller.cart.js
+++ HC_Webapp/src/controllers/controller.cart.js
@@ -16,36 +16,14 @@ const controllerCart = {
     let userLoggedIn = readLoggedUser();
     let productList = indexProduct();
     let i = req.params.id;
-    res.render('products/cartDetail', {
+    res.render('products/cart', {
       productList: productList,
       i: i,
-      userLoggedIn: userLoggedIn,
-    });
-  },mostrarProductos: (req, res) => {
-    //Llama a la funcion indexProduct() en products.model.js para armar el array de productos en productList.
-    productList = indexProduct();
-    //Llama a la funcion readLoggedUser en products.model.js para armar el array del usuario logueado en users.model (aunque este vacio).
-    userLoggedIn = readLoggedUser();
-    //Retorna la vista "home" junto a las variables declaradas productList y userLoggedIn para que la vista muestre los valores que correspondan.
-    return res.render('products/cart', {
-      productList: productList,
       userLoggedIn: userLoggedIn,
     });
   },
   //TODO falta terminar este metodo
   comprarProduct: (req, res) => {
-    let productList = indexProduct();
-    let userLoggedIn = readLoggedUser();
-    let i = req.params.id;
-    res.render('products/productCreate', {
-      productList: productList,
-      i: i,
-      userLoggedIn: userLoggedIn,
-    });
-  },
-
-
-  crearProducto: (req, res) => {
     let productList = indexProduct();
     let userLoggedIn = readLoggedUser();
     let i = req.params.id;
--- HC_Webapp/src/routes/cart.routes.js
+++ HC_Webapp/src/routes/cart.routes.js
@@ -10,29 +10,9 @@ const {
   updateProduct,
 } = require('../model/cart.model');
 
-router.get('/', controllerCart.mostrarProductos);
+router.get('/', controllerCart.mostrarProducto);
 
-router.get('/:id/mostrar', controllerCart.mostrarProducto);
-
-router.put('/editar', (req, res) => {
-  updateProduct(req.body);
-  res.redirect('/producto/' + req.body.id + '/mostrar');
-}); 
-
-router.get('/crear', controllerCart.crearProducto);
-
-router.post('/crear', uploadProduct.single('image'), (req, res) => {
-  let imageCheck = '';
-  let productList = indexProduct();
-  if (!req.file) {
-    imageCheck = 'noproduct.png';
-  } else {
-    imageCheck = req.file.filename;
-  }
-  productList.push(comprarProduct(req.body, imageCheck));
-  writeProductJSON(cartList);
-  res.redirect('/producto/' + req.body.id + '/mostrar');
-});
+router.get('/:id/mostrarCart', controllerCart.mostrarProducto);
 
 router.get('/:id/eliminar', controllerProducto.eliminarProducto);
 
