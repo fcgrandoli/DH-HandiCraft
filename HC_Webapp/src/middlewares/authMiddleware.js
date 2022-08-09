b36546632824dc9a42486c682c321f2e83380166
fcgrandoli <82011284+fcgrandoli@users.noreply.github.com>
2022-08-09 01:23:15 -0300

    Corrigiendo los mid de auth y guest.

--- HC_Webapp/src/middlewares/authMiddleware.js
+++ HC_Webapp/src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ function authMiddleware(req, res, next) {
   if (req.session.user != undefined) {
     next();
   } else {
-    res.send('Solo para usuarios');
+    res.send('Solo para usuarios registrados.');
   }
 }
 module.exports = authMiddleware;
--- HC_Webapp/src/middlewares/guestMiddleware.js
+++ HC_Webapp/src/middlewares/guestMiddleware.js
@@ -2,7 +2,7 @@ function guestMiddleware(req, res, next) {
   if (req.session.user == undefined) {
     next();
   } else {
-    res.send('Solo para invitados');
+    res.send('Solo para usuarios no registrados.');
   }
 }
 module.exports = guestMiddleware;
