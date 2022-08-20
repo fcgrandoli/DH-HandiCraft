b5ce0810a7cccb9be208b26adff7d5784e9cfcb3
santuboca2001 <santipaeztoledo@hotmail.com>
2022-08-20 16:03:39 -0300

    Config database

---
+++ HC_Webapp/src/database/models/users.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = "users";
+    let cols={
+        id:{
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey : true,
+            type: DataTypes.INTEGER
+        },
+        username: {
+            type:DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.TEXT
+        },
+        avatar: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+
+    };
+    let config = {
+        timestamps:false,
+        deletedAt: false
+    };
+
+    const User = sequelize.define(alias, cols, config)
+    return User;
+}
