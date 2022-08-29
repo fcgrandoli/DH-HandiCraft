79c276eb46899ae929d4cee08d2e478b454a762f
santuboca2001 <santipaeztoledo@hotmail.com>
2022-08-29 15:38:15 -0300

    isAdmn de tabla users actualizado

--- HC_Webapp/src/database/models/users.js
+++ HC_Webapp/src/database/models/users.js
@@ -2,37 +2,37 @@
     let alias = "User";
     let cols={
         id: {
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: DataTypes.INTEGER
-          },
-          first_name: {
-            type: DataTypes.STRING
-          },
-          last_name: {
-            type: DataTypes.TEXT
-          },
-          user_name: {
-            type: DataTypes.STRING
-          },
-          email : {
-            type: DataTypes.STRING
-          },
-          passwd : {
-            type: DataTypes.STRING
-          },
-          avatar: {
-            type: Sequelize.INTEGER,
-            allowNull:true,
-            references:{
-              model:'images',
-              key:'id'}
-          },
-          isAdmin: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-          }
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER
+        },
+        first_name: {
+          type: DataTypes.STRING
+        },
+        last_name: {
+          type: DataTypes.TEXT
+        },
+        user_name: {
+          type: DataTypes.STRING
+        },
+        email: {
+          type: DataTypes.STRING
+        },
+        passwd: {
+          type: DataTypes.STRING
+        },
+        isAdmin: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: false
+        },
+        avatar: {
+          type: DataTypes.TEXT,
+          allowNull:true
+        },
+        loggedIn: {
+          type: DataTypes.BOOLEAN
+        }
 
     };
     let config = {
