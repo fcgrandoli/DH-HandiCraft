d7eef0e21ab54166ba31bc5804ecbd63decd4a7f
CarolinaShlain123 <caroshlain@gmail.com>
2022-09-17 23:40:25 -0300

    validaciones del producto terminadas

--- HC_Webapp/public/validationsFront/productCreate.js
+++ HC_Webapp/public/validationsFront/productCreate.js
@@ -1,26 +1,89 @@
-window.addEventListener('load', function(){ 
-    let formulario = document.querySelector('form.fields');
+let forms = document.forms.news ;
 
-formulario.addEventListener('submit', function(evento){
-    
+let inputs = forms.elements;
 
-let nombre = document.querySelector('input.name');
-if (nombre.value == ""){
-    msg('el campo de nombre no puede estar vacio')}
 
-else if (nombre.value.length < 5){
-   msg('el campo tiene que tener minimo 5 caracteres')}
-
-let descripcion = document.querySelector('input.descLarge')
-if (descripcion.value.length < 20){
-    msg('el campo tiene que tener minimo 20 caracteres')}
-
-    if (!['jpg', 'jpeg', 'png', 'gif'].includes(files[0].type.split('/')[1])){
-        msg('No es un formato valido')
+inputs.name.addEventListener('input', function (e) { 
+    let field = e.target.parentElement;
+    let value = e.target.value;
+    let feed = field.querySelector('.feed');
+    let msg = null;
+    if (!validator.isLength(value, { min: 5 })) {
+        msg = 'El nombre debe contener mínimo cinco caracteres.'
+    } else {
+        validmsg = 'El campo es correcto.'
+    }
+    if (msg) {
+        field.classList.remove('valid');
+        field.classList.add('invalid');
+        feed.innerText = msg
+    }
+    else {
+        field.classList.remove('invalid');
+        field.classList.add('valid');
+        feed.innerText = validmsg
     }
 
 })
-}
-)
 
+inputs.descLarge.addEventListener('input', function (e) {     
+    let field = e.target.parentElement;
+    let value = e.target.value;
+    let feed = field.querySelector('.feed');
+    let msg = null;
+    let validmsg = field.querySelector('.feed-valid');      
+    if (!validator.isLength(value, { min: 20 })) {
+        msg = 'La descripcion larga debe contener mínimo veinte caracteres.'
+    } else {
+        validmsg = 'El campo es correcto.'
+    }
+    if (msg) {
+        field.classList.remove('valid');
+        field.classList.add('invalid');
+        feed.innerText = msg
+    }
+    else {
+        field.classList.remove('invalid');
+        field.classList.add('valid');
+        feed.innerText = validmsg
+    }
+
+})
+inputs.avatar.addEventListener('input', function (e) {
+    let field = e.target.parentElement;
+    let files = e.target.files;
+    let feed = field.querySelector('.feed');
+    let msg = null;
+    let validmsg = field.querySelector('.feed-valid');
+    if (files.length == 0) {
+        msg = 'El campo de avatar esta vacio.'
+    }
+    else if (!['jpg', 'jpeg', 'png', 'gif'].includes(files[0].type.split('/')[1])) {
+        msg = 'No es un formato de imagen valida.'
+    } else {
+        validmsg = 'El campo es correcto'
+    }
+    if (msg) {
+        field.classList.remove('valid');
+        field.classList.add('invalid');
+        feed.innerText = msg
+    } else {
+        field.classList.remove('invalid');
+        field.classList.add('valid');
+        feed.innerText = validmsg
+    }
+})
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
 
