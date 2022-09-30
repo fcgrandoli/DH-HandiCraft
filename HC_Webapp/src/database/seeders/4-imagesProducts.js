363cd9dbdf4c049194c809fc4b31efb2a37065e6
fcgrandoli <82011284+fcgrandoli@users.noreply.github.com>
2022-09-29 21:40:20 -0300

    Arreglando Seeder de ImagesProducts

--- HC_Webapp/src/database/seeders/4-imagesProducts.js
+++ HC_Webapp/src/database/seeders/4-imagesProducts.js
@@ -1,117 +1,15 @@
-"use strict";
-
+'use strict';
+const { indexProduct } = require('../../model/products.model');
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      "imagesProducts",
-      [
-        {
-          product: 1,
-          image: 1,
-        },
-        {
-          product: 2,
-          image: 2,
-        },
-        {
-          product: 3,
-          image: 3,
-        },
-        {
-          product: 4,
-          image: 4,
-        },
-        {
-          product: 5,
-          image: 5,
-        },
-        {
-          product: 6,
-          image: 6,
-        },
-        {
-          product: 7,
-          image: 7,
-        },
-        {
-          product: 8,
-          image: 8,
-        },
-        {
-          product: 9,
-          image: 9,
-        },
-        {
-          product: 10,
-          image: 10,
-        },
-        {
-          product: 11,
-          image: 11,
-        },
-        {
-          product: 12,
-          image: 12,
-        },
-        {
-          product: 13,
-          image: 13,
-        },
-        {
-          product: 14,
-          image: 14,
-        },
-        {
-          product: 15,
-          image: 15,
-        },
-        {
-          product: 16,
-          image: 16,
-        },
-        {
-          product: 17,
-          image: 17,
-        },
-        {
-          product: 18,
-          image: 18,
-        },
-        {
-          product: 19,
-          image: 19,
-        },
-        {
-          product: 20,
-          image: 20,
-        },
-        {
-          product: 21,
-          image: 21,
-        },
-        {
-          product: 22,
-          image: 22,
-        },
-        {
-          product: 23,
-          image: 23,
-        },
-        {
-          product: 24,
-          image: 24,
-        },
-        {
-          product: 25,
-          image: 25,
-        },
-
-      ],
-      {}
-    );
+    let products = indexProduct().map(product => {
+      delete product.image;
+      return Object({ product: product.id, image: product.id });
+    });
+    await queryInterface.bulkInsert('imagesProducts', products, {});
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("imagesProducts", null, {});
+    await queryInterface.bulkDelete('imagesProducts', null, {});
   },
 };
