e1c3248e5627bd513e43d57929534b80ea503eaa
CarolinaShlain123 <caroshlain@gmail.com>
2022-09-29 23:50:23 -0300

    creando carpetas de react

---
+++ Dashboard/src/services/users.js
@@ -0,0 +1,25 @@
+const baseURL = "http://localhost:3000/api/users" // api y user y products
+
+/* export async function getAll(){
+    try{
+    let endpoint = `${baseURL}` //nada en vez de page
+    let query = await fetch (endpoint)
+    let data = await query.json()
+    return data.results 
+} catch (error){
+    console.log(error)
+    return []
+}
+}
+
+export async function getOne(id){
+    try{
+    let endpoint = `${baseURL}/${id}`
+    let query = await fetch (endpoint)
+    let data = await query.json()
+    return data.results
+} catch (error){
+    console.log(error)
+    return []
+}
+} */
