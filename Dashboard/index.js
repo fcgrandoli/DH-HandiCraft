4a4f5965ee8f4bf28a296ef360736d753d115df1
santuboca2001 <santipaeztoledo@hotmail.com>
2022-09-30 16:03:21 -0300

    app.jsx funcionando

---
+++ Dashboard/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Home(){
+  return (
+    <React.Fragment>
+    <title>Handicraft</title>
+  </React.Fragment>
+)
+}
+export default Home;
--- HC_Webapp/src/controllers/apis/userAPI.js
+++ HC_Webapp/src/controllers/apis/userAPI.js
@@ -3,58 +3,58 @@ const { Op } = require("sequelize");
 
 
 const userApi = {
-    list: async (req, res) => {
-      try {
-        let result = [];
-        let count = await user.findAll({
-            include: {
-                all: true
-            },
-        })
-       
-        let data = count.map(user => Object({
-          id:user.id,
-          Nombre: user.firstName,
-          Email: user.email,
-          Detalle: "http://localhost:3000/api/users/" + user.id
+  list: async (req, res) => {
+    try {
+      let result = [];
+      let count = await user.findAll({
+        include: {
+          all: true
+        },
       })
-  )
-  result.push ({
-    'Cantidad total de usuarios': count.length,
-    Usuarios : data
-  })
 
-        if (result) {
-          return res.status(200).json(result);
-        } else {
-          return res.status(404).json('No hay usuarios.');
-        }
-      } catch (error) {
-        return res.status(500).json(error);
+      let data = count.map(user => Object({
+        id: user.id,
+        Nombre: user.firstName,
+        Email: user.email,
+        Detalle: "http://localhost:3000/api/users/" + user.id
+      })
+      )
+      result.push({
+        'Cantidad total de usuarios': count.length,
+        Usuarios: data
+      })
+
+      if (result) {
+        return res.status(200).json(result);
+      } else {
+        return res.status(404).json('No hay usuarios.');
       }
-    },
-    detail: async (req, res) => {
-      try {
-        let result = await user.findByPk(req.params.id, {
-          include: {
-            all: true,
-          },
-        })
-        let data = {}
-        data.id = result.id;
-        data.firstName = result.firstName;
-        data.userName = result.userName;
-        data.avatar = "http://localhost:3000/assets/avatars/" + result.avatar
-        if (data) {
-          return res.status(200).json(data);
-       } else {
-          return res.status(404).json("Este usuario no existe.");
-       }
-      } catch (error) {
-        return res.status(500).json(error);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  },
+  detail: async (req, res) => {
+    try {
+      let result = await user.findByPk(req.params.id, {
+        include: {
+          all: true,
+        },
+      })
+      let data = {}
+      data.id = result.id;
+      data.firstName = result.firstName;
+      data.userName = result.userName;
+      data.avatar = "http://localhost:3000/assets/avatars/" + result.avatar
+      if (data) {
+        return res.status(200).json(data);
+      } else {
+        return res.status(404).json("Este usuario no existe.");
+      }
+    } catch (error) {
+      return res.status(500).json(error);
     }
   }
-  }
+}
 
 
 
