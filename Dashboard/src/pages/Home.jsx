5e83b60e38775dc55a6fc61c5fc2a526e23502fc
santuboca2001 <santipaeztoledo@hotmail.com>
2022-10-02 23:18:31 -0300

    app

--- Dashboard/src/pages/Home.jsx
+++ Dashboard/src/pages/Home.jsx
@@ -5,30 +5,29 @@ export default function Home() {
 
     <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar" >
 
-                {/*<!-- Sidebar - Brand -->*/}
+             
                 <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
                     <div className="sidebar-brand-icon">
                         <img className="w-100" src={image} height="70px" width="80px" alt="Digital House"/>
                     </div>
                 </a>
 
-                {/*<!-- Divider -->*/}
+            
                 <hr className="sidebar-divider my-0"/>
 
-                {/*<!-- Nav Item - Dashboard -->*/}
+               
                 <li className="nav-item active">
                     <a className="nav-link" href="/">
                     
                         <span class="sub">HANDICRAFT</span></a>
                 </li>
 
-                {/*<!-- Divider -->*/}
                 <hr className="sidebar-divider"/>
 
-                {/*<!-- Heading -->*/}
+              
                 <div className="sidebar-heading">Contenido</div>
 
-                {/*<!-- Nav Item - Pages -->*/}
+                
                 <li className="nav-item">
                     <a className="nav-link collapsed" href="/products">
                         <i className="fas fa-fw fa-folder"></i>
@@ -36,21 +35,21 @@ export default function Home() {
                     </a>
                 </li>
 
-                {/*<!-- Nav Item - Charts -->*/}
+              
                 <li className="nav-item">
                     <a className="nav-link" href="/users">
                         <i className="fas fa-fw fa-chart-area"></i>
                         <span>Usuarios</span></a>
                 </li>
 
-                {/*<!-- Nav Item - Tables -->*/}
+               
                 <li className="nav-item">
                     <a className="nav-link" href="/">
                         <i className="fas fa-fw fa-table"></i>
                         <span>Categorias</span></a>
                 </li>
 
-                {/*<!-- Divider -->*/}
+                
                 <hr className="sidebar-divider d-none d-md-block"/>
             </ul>
 
--- Dashboard/src/pages/Users.jsx
+++ Dashboard/src/pages/Users.jsx
@@ -1,9 +1,8 @@
-
 import { useState, useEffect} from "react"
+import { Outlet, useParams } from "react-router-dom"
 
 
 const Users = () => {
-<<<<<<< HEAD
 let [count, setUser] = useState([])
 useEffect(()=>{
     const userApi = async () => {
@@ -21,39 +20,6 @@ return (
     <p>   
     </p>
 </>
-=======
-
-    const url = "http://localhost:3000/api/users" ;
-
-      const fetchApi = async () => 
-       {
-         let request = await fetch(url);
-
-        let response = await request.json()
-
-        setUsers(response)
-             
-         console.log(response)
- }
- useEffect(() => {
-     fetchApi();
- }, []);
-
-
-
-return (
-
-    <>
-    <h1>
-    Total usuarios:
-    </h1>
-    <p>
-    {
-    
-    }
-    </p>
-    </>
->>>>>>> 85d7271 (sidebar dashboard)
 )
 }
 
@@ -61,26 +27,6 @@ return (
 
 
 
-
-
-
-
-
-// const Users = () => {
-// let [count, setUser] = useState([])
-// useEffect(()=>{
-//     const userApi = async () => {
-//         let request = await fetch('http://localhost:3000/api/users', {
-//             mode: 'no-cors'
-//         }
-//         )
-//         let response = await request.json()
-//         setUser(response)
-//         console.log(response)
-        
-// }
-// userApi()
-// }, [])
 
 export default Users
 
