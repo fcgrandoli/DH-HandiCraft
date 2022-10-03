c497bbbaa2c8fb0dc83ce386bb25f4671ac86200
CarolinaShlain123 <caroshlain@gmail.com>
2022-10-02 23:40:27 -0300

    ,

--- Dashboard/src/pages/Products.jsx
+++ Dashboard/src/pages/Products.jsx
@@ -16,32 +16,10 @@ productApi()
 return (
 <>
     <h1>Total productos:</h1>
-    <p>   
+    <p>  
+       
     </p>
 </>
 )
 }
 export default Products
-
-
-/* import { useState, useEffect} from "react"
-import { Outlet } from "react-router-dom"
-
-const Products = () => {
-let [count, setProducts] = useState(null)
-    const productApi = async () => {
-        let request = await fetch('http://localhost:3000/api/products')
-        let response = await request.json();
-       return responese;
-}
-useEffect(productApi,[])
-return (
-<>
-    <h1>Total productos:</h1>
-    <Outlet />
-    <p>   
-    </p>
-</>
-)
-}
-export default Products */
